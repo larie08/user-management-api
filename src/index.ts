5ca377733f403f703a83e0334302ecdb33db16d2
MGB7 IO Heart Rizarri <mariannemaecalderon04@gmail.com>
2025-03-30 19:11:19 +0800

    Final commit with resolved conflicts in src folder

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 // Global error handler (must be last!)
 app.use(errorHandler);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(Server running on port ${port});
 });
