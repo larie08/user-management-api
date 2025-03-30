475a46dc73222a6f7b48ec283a582b84fff225c4
MGB7 IO Heart Rizarri <mariannemaecalderon04@gmail.com>
2025-03-30 19:17:58 +0800

    Resolve merge conflict in src folder

--- src/index.ts
+++ src/index.ts
@@ -34,5 +34,5 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(Server running on port ${port});
+  console.log(`Server running on port ${port}`);
 });
